bc18beaee1902e8c6e274247ec02bb8fb84c00b8
Vivian Nguyen <vivianyn.co@gmail.com>
2024-05-15 10:05:25 -0400

    Made the website SEO friendly, improved UI, added Typewriter effect

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  new Typewriter("#typewrite-name", {
+    strings: "> Vivian Nguyen",
+    autoStart: true,
+    delay: 100,
+    loop: true,
+    pauseFor: 3000,
+  });
+});
